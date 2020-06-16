95950ace2507fc349030feadee611f1d035f40f4
Ahmedkoga <valentino-koga@hotmail.com>
2020-06-16 23:31:31 +0200

    st commit

---
+++ environment/production.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+    dbUrl: 'mongodb+srv://max:free@cluster0-p1f9v.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    cert: '/etc/letsencrypt/live/medi-taxi.fr/fullchain.pem',
+    key:  '/etc/letsencrypt/live/medi-taxi.fr/privkey.pem',
+    portHttp: 80,
+    portHttps: 443
+  }
